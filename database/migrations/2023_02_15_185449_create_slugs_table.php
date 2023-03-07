af0b9d792bee4bb77129933baddadb0dfd9a4009
Vlad Osmianski <vlaosm@gmail.com>
2023-03-07 17:33:06 +0200

    `users`, `organizations`, `organization_user`, `accounts`, `slugs` tables

---
+++ database/migrations/2023_02_15_185449_create_slugs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slugs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('slug')->unique();
+            $table->string('type', 20);
+
+            $table->foreignId('organization_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slugs');
+    }
+};
